48c27d2bf836fc8bda8a9c8a09f222cf82d0f939
econtrer <eduardo.contrerasd@gmail.com>
2026-01-03 17:58:09 -0300

    Asteroids spawning

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from player import Player
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from constants import *
 from logger import log_state
 
 def main():
@@ -17,9 +19,14 @@ def main():
     # On screen objects setup
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     while True:
         log_state()
