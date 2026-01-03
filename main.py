4aa1ac7e872d00b3a608b6ab3e4e4193eb51cbbe
econtrer <eduardo.contrerasd@gmail.com>
2026-01-03 17:27:24 -0300

    Using groups for drawing and updating

--- main.py
+++ main.py
@@ -14,7 +14,11 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
-    # 
+    # On screen objects setup
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -24,8 +28,10 @@ def main():
                 return
         screen.fill("black")
 
-        player.draw(screen)
-        player.update(dt)
+        for d in drawable:
+            d.draw(screen)
+        
+        updatable.update(dt)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
