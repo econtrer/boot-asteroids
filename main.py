d26ff2cddd383df9c53c840c683395a26f11505e
econtrer <eduardo.contrerasd@gmail.com>
2026-01-03 16:15:24 -0300

    Player class, draw it as a triangle on screen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
@@ -7,10 +8,14 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     
+    # Pygame setup
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    # 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -18,6 +23,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+
+        player.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
